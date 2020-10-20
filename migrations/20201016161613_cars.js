f2be2c85e43158a477d57f79282d61c82397fd66
Brian Bagrov <Brianbagrov@gmail.com>
2020-10-20 16:48:13 -0400

    MVP reached.

---
+++ migrations/20201016161613_cars.js
@@ -0,0 +1,16 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("cars", (table) => {
+table.increments("id");
+table.text("VIN").notNull().unique();
+table.text("make").notNull();
+table.text("model").notNull();
+table.integer("mileage").notNull();
+table.text("transmission").notNull();
+table.text("status").defaultTo(null);
+  })
+  
+};
+
+exports.down = function(knex) {
+  
+};
